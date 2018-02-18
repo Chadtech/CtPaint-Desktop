cd260588e9f4c48b0481b3d6926930066ce4d369
Chadtech <chadstearns1@gmail.com>
2018-02-18 17:11:19 +0100

    More content to the tos

--- source/Tos.elm
+++ source/Tos.elm
@@ -151,7 +151,7 @@ definitionsContent =
 
 termsContent : String
 termsContent =
-    "By accessing the CtPaint website, you are agreeing to be bound by these terms and conditions of use, all applicable laws and regulations, and agree that you are responsible for compliance with those those laws and regulations. If you do not agree with any of these terms, you are prohibited from using this site."
+    "By accessing the CtPaint website, you are agreeing to be bound by these terms and conditions of use, all applicable laws and regulations, and agree that you are responsible for compliance with those those laws and regulations. You agree to this regardless as to whether you affirmatively agree to the terms of service, as such when you do on the account registration page. If you do not agree with any of these terms, you are prohibited from using this site. The terms of service may change at any time, and in the event of a change, the change will be listed on the CtPaint website, and a notice will be sent to all registered CtPaint users."
 
 
 disclaimerContent : String
@@ -173,9 +173,10 @@ privacyList : List String
 privacyList =
     [ "CtPaint is developed using Amazon Web Services. All authentication is done using Amazon Web Services, and the best practices for using it. All user data is stored at Amazon through Amazon Web Services"
     , "Chadtech does not store your password and does not know what it is. Chadtech cannot access your password from your user data on the Amazon Web Services servers. During log in, passwords are cleared from memory immediately after the user attempts to log in."
+    , "If you opt into local storage, then your files will be stored locally on your computer, and could therefore be possibly accessed by other users on the same computer if they searched your computer browsers local memory without your permission, but not by any feature of CtPaint. If you opt into local storage a notice of this will be shown."
     , "Chadtech will never give out user identifying or personal information without the consent of the identified users, unless compelled to by law."
-    , "Chadtech may look at your email address and may email you."
+    , "Chadtech may look at your email address and may email you. The registration page of CtPaint asks if you would like to sometimes receive emails. If you choose not to, you will not receive emails about non-urgent things like updates or new feautres to CtPaint, but you may still receive emails about changes to the terms of service or in regard to other important matters."
     , "Chadtech may look at user data in an anonymized and aggregated way. Anonymizing user data means removing information that could be used to discover the identity of the user, such as emails, profile bios, usernames, user ids, and image data."
-    , "Chadtech will never look at private user data without the permission of the user. Private user data in this context, means data only the user could access through the normal operation of CtPaint. Private images would be an example."
+    , "Chadtech will never look at your private user data without your permission. Private user data in this context, means data only the user could access through the normal operation of CtPaint. Private images would be an example."
     , "All drawings on CtPaint, except private drawings, which are only accessible to the silver subscription tier, are publicly accessible. This means that anyone with the corresponding url could access it."
     ]
